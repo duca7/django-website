d9b0d3d456eedf7ee316df0f686f59bd0d4ac296
duc <hohuynhduca7bh123@gmail.com>
2021-05-22 10:31:52 +0700

    init project

---
+++ sedjango/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sedjango project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sedjango.settings')
+
+application = get_wsgi_application()
