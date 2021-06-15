ed12a5a17e709639318f52738ff90ebfbee68916
duc <hohuynhduca7bh123@gmail.com>
2021-06-15 09:16:03 +0700

    remove all, start new

--- orders/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Order
-
-
-class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ['first_name', 'last_name', 'phone', 'email', 'address', 'order_note']
--- orders/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('place_order/', views.place_order, name='place_order'),
-]
